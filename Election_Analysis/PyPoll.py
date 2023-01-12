d5d5642ab7b1a1d8fe0f819cb44a810c1ec56883
jkehm <73975096+jkehm@users.noreply.github.com>
2023-01-12 00:30:38 -0500

    Add files via upload

--- Election_Analysis/PyPoll.py
+++ Election_Analysis/PyPoll.py
@@ -1,24 +1,15 @@
-#Add our dependencies
+# Add our dependencies.
 import csv
 import os
 
-# Assign a variable to save a file from a path
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Assign a variable for the file to load a path. 
+# Assign a variable to load/save a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-    # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Print each row in the CSV file.
-    for row in file_reader:
-        print(row)
-
-
-
-# Close File
-    print(election_data)
-
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
